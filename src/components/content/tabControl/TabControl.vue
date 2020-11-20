c717befc2fc03be383542c26badd4111d8db15b6
JUN-1999 <1173943332@qq.com>
2020-11-20 21:04:41 +0800

    2020/11/20 完成首页的布局

---
+++ src/components/content/tabControl/TabControl.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="tab-control">
+    <div
+      v-for="(item, index) in titles"
+      :key="index"
+      class="tab-control-item"
+      :class="{ active: index === currentIndex }"
+      @click="changeIndex(index)"
+    >
+      <span>{{ item }}</span>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "TabControl",
+  data() {
+    return {
+      currentIndex: 0
+    };
+  },
+  methods: {
+    changeIndex(index) {
+      this.currentIndex = index;
+      this.$emit("tabClick", index);
+    }
+  },
+  props: {
+    titles: {
+      type: Array,
+      default() {
+        return [];
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+.tab-control {
+  display: flex;
+  text-align: center;
+  height: 40px;
+  line-height: 40px;
+  background-color: #fff;
+}
+
+.tab-control-item {
+  flex: 1;
+}
+.tab-control-item span {
+  padding: 5px;
+}
+
+.active {
+  color: pink;
+}
+.active span {
+  border-bottom: 3px solid pink;
+}
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,44 +1,204 @@
 <template>
-  <div>
+  <div id="home">
     <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
     <home-swiper :banners="banners"></home-swiper>
     <recommend-view :recommends="recommends"></recommend-view>
+    <feature></feature>
+    <tab-control
+      class="tab-control"
+      :titles="['流行', '新款', '精选']"
+      @tabClick="tabClick"
+    ></tab-control>
+    <goods-list :goods="showGoods"></goods-list>
+    <ul>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+    </ul>
   </div>
 </template>
 <script>
-import NavBar from "components/common/navbar/NavBar";
 import HomeSwiper from "./childComps/HomeSwiper";
 import RecommendView from "./childComps/RecommendView";
+import Feature from "./childComps/Feature";
 
-import { getHomeMultidata } from "network/home";
+import NavBar from "components/common/navbar/NavBar";
+import TabControl from "components/content/tabControl/TabControl";
+import GoodsList from "components/content/goods/GoodsList";
+
+import { getHomeMultidata, getHomeGoods } from "network/home";
 
 export default {
   name: "Home",
   components: {
-    NavBar,
     HomeSwiper,
-    RecommendView
+    RecommendView,
+    Feature,
+    NavBar,
+    TabControl,
+    GoodsList
   },
   data() {
     return {
       banners: [],
-      recommends: []
+      recommends: [],
+      goods: {
+        pop: { page: 0, list: [] },
+        new: { page: 0, list: [] },
+        sell: { page: 0, list: [] }
+      },
+      currentType: "pop"
     };
   },
+  computed: {
+    showGoods() {
+      return this.goods[this.currentType].list;
+    }
+  },
   created() {
+    this.getHomeMultidata();
+    // 请求商品数据
+    this.getHomeGoods("pop");
+    this.getHomeGoods("new");
+    this.getHomeGoods("sell");
+  },
+  methods: {
+    // ///////////
+    // 事件监听相关的方法
+    // ///////////
+
+    tabClick(index) {
+      switch (index) {
+        case 0:
+          this.currentType = "pop";
+          break;
+        case 1:
+          this.currentType = "new";
+          break;
+        case 2:
+          this.currentType = "sell";
+          break;
+      }
+    },
+
+    // ////////////
+    //  网络请求相关
+    // ///////////
     //   1.请求多个数据
-    getHomeMultidata().then(res => {
-      this.banners = res.data.banner.list;
-      this.recommends = res.data.recommend.list;
-    });
+
+    getHomeMultidata() {
+      getHomeMultidata().then(res => {
+        this.banners = res.data.banner.list;
+        this.recommends = res.data.recommend.list;
+      });
+    },
+    // 商品列表
+    getHomeGoods(type) {
+      const page = this.goods[type].page + 1;
+      getHomeGoods(type, page).then(res => {
+        this.goods[type].list.push(...res.data.list);
+        this.goods[type].page += 1;
+      });
+    }
   }
 };
 </script>
 
 <style scoped>
+#home {
+  padding-top: 44px;
+}
+
 .home-nav {
   background-color: #ff8e96;
   color: aliceblue;
   font-weight: 900;
+
+  position: fixed;
+  left: 0;
+  top: 0;
+  right: 0;
+  z-index: 99;
+}
+
+.tab-control {
+  position: sticky;
+  top: 44px;
+  z-index: 9;
 }
 </style>
